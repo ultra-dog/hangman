8235b9795e226962a3f2960927e2ffac9497b5e7
SomeGiraffe <50085812+SomeGiraffe@users.noreply.github.com>
2019-05-19 16:19:42 -0400

    Update word_picker.py

--- word_picker.py
+++ word_picker.py
@@ -1,1 +1,9 @@
 # word picker code
+def hangman():
+    import random
+    l = list["giraffe", "python", "zebra", "lion", "squirrel"]
+    for x in range(1):
+        random.randint(1,5)
+    word = l[x]
+
+
